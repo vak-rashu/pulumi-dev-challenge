395a2d0134b7229c54d013a3ebf7f7fa3fb64e6e
vak-rashu <rashu.cloud11@gmail.com>
2025-04-03 21:55:25 +0530

    Create __main__.py

---
+++ pulumi-esc/__main__.py
@@ -0,0 +1,34 @@
+import pulumi
+import pulumi_aws as aws
+
+# Create IAM role for Lambda
+lambda_role = aws.iam.Role("lambdaRole",
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {"Service": "lambda.amazonaws.com"},
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }""")
+
+# Attach AWS Lambda basic execution role policy
+role_policy_attachment = aws.iam.RolePolicyAttachment("lambdaPolicyAttach",
+    role=lambda_role.name,
+    policy_arn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+)
+
+# Lambda function code
+lambda_function = aws.lambda_.Function("ssm-store-api",
+    runtime="python3.12",
+    handler="lambda.handler",
+    role=lambda_role.arn,
+    code=pulumi.AssetArchive({
+        ".": pulumi.FileArchive("./api_infra") 
+    })
+)
+
+pulumi.export("lambda_function", lambda_function.name) 
