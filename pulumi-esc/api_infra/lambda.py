956cc4ccd75bc59276310c419bb2f8caeba5697e
vak-rashu <rashu.cloud11@gmail.com>
2025-04-03 21:59:20 +0530

    Create lambda.py

---
+++ pulumi-esc/api_infra/lambda.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+import json
+import requests
+from api_infra.config import api_key, city
+
+endpoint_url = "http://localhost:4566"
+
+ssm_client = boto3.client("ssm",
+            endpoint_url = endpoint_url,
+            aws_access_key_id = 'test', 
+            aws_secret_access_key = 'test',
+            region_name = 'us-east-1',  
+)
+
+def handler(event, context):
+        try:
+            api_key = api_key
+            city_val = city
+            
+            #getting value from the url
+            base_url = f"https://api.weatherstack.com/current?access_key={api_key}&query={city_val}"
+            response = requests.get(base_url)
+            body = response.json()
+            temperature = body["current"]["temperature"]
+
+            #describing parameters
+            parameter_name = "/weather/temperature_value"
+            parameter_value = temperature
+
+            #using ssm client to put the paratmets 
+            response = ssm_client.put_parameter(
+            Name=parameter_name,
+            Value=parameter_value,
+            Type="String",  
+            Overwrite=True 
+        )
+            body = json.dumps({
+                "message": "value added successfully"
+            })
+
+        except Exception as e:
+            body = json.dumps({
+                "error": e
+            })
+        return body
