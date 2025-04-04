4ddbe35d9698a66c6e7d2705f00772626ebce278
vak-rashu <rashu.cloud11@gmail.com>
2025-04-04 10:50:26 +0530

    Update config.py

--- pulumi-esc/api_infra/config.py
+++ pulumi-esc/api_infra/config.py
@@ -4,5 +4,6 @@ import pulumi
 config = pulumi.Config()
 
 # Retrieve the values of "api-key" and "city"
+endpoint_url = config.get("endpoint_url")
 api_key = config.get("api-key")
 city = config.get("city")
